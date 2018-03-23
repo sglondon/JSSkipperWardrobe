466874925bb89d96d7b9fe456b8671f2f9cf87a2
sglondon <susangs2@gmail.com>
2018-03-23 15:13:13 -0400

    steps 1 and 1/2 done

---
+++ WardrobeApp/Content/JavaScript.js
@@ -0,0 +1,26 @@
+﻿//On loading the home index page, greet the user, get their name and display it
+
+function Greeting() {
+    var x = document.getElementById("jumbomain");
+    var userResponse = prompt("Welcome to Skipper's Wardrobe. Please enter your name?");
+    x.getElementsByTagName('h2')[0].innerText = "Welcome to Skipper's Wardrobe " + userResponse;
+}
+
+//change the color and size of the font when mousing over Meet Skipper
+
+
+
+
+
+
+function mouseOver() {
+    var x = document.getElementById("meetSkipper")
+    document.getElementById("meetSkipper").style.color = "red" ;
+    
+
+}
+
+function mouseOut() {
+    document.getElementById("meetSkipper").style.color = "black";
+}
+
