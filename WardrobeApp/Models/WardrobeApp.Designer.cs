0896fd4b4bd93e2e346bfa57d319de6cdb4c8691
sglondon <susangs2@gmail.com>
2018-04-18 10:43:57 -0400

    added connection strings

--- WardrobeApp/Models/WardrobeApp.Designer.cs
+++ WardrobeApp/Models/WardrobeApp.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\WeCanCodeIT\source\repos\WardrobeApp\WardrobeApp\Models\WardrobeApp.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Owner\Desktop\JSSkipperWardrobe\WardrobeApp\Models\WardrobeApp.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
